82a03f05636c1f48601351d6ad21ac685ad14329
lingtouyangLeo <tl3191@nyu.edu>
2025-03-27 01:07:26 -0400

    typo

--- ui/src/components/AddTaskForm.js
+++ ui/src/components/AddTaskForm.js
@@ -2,19 +2,27 @@ import React, {useState} from 'react'
 import TextField from "@mui/material/TextField";
 import { Button, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import axios from 'axios';
+import { API_URL } from '../utils';
 
 export const AddTaskForm = () => {
 
     const [newTask, setnewTask] = useState("")
-    const addNewTask = () => {
-        console.log("Adding new task")
+    const addNewTask = async ({fetchTasks}) => {
+        try {
+            await axios.post(API_URL, {name: newTask, completed: false});
+            await fetchTasks();
+            setnewTask("")
+        } catch (err) {
+            console.log(err)
+        }
     }
     return (
         <div className='add-task-form'>
             <Typography align="center" variant="h2" paddingTop={2} paddingBottom={2} sx={{ width: "100%" }}>
             My Task List 
             </Typography>
-           <div>    
+           <div className='inputrow'>    
                 <TextField size="small" label="Task" variant="outlined" value={newTask} onChange={(e)=>setnewTask(e.target.value)}/>
                 <Button disabled={!newTask.length} variant='outlined' onClick={addNewTask}>
                     <AddIcon />
